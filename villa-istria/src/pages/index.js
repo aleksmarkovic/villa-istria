5e0924660cea04982ba1a7d3241f229e2ecb6e2e
Aleks Markovic <aleks.markovic182@gmail.com>
2022-04-25 18:05:45 +0200

    Remove construction import

--- villa-istria/src/pages/index.js
+++ villa-istria/src/pages/index.js
@@ -3,8 +3,6 @@ import Header from "../components/Layout/Header/Header";
 import Footer from "../components/Layout/Footer/Footer";
 import HomeMain from "../components/Home/HomeMain";
 
-import ConstructionPage from "./construction-in-progress";
-
 const Index = (props) => {
   return (
     <React.Fragment>
