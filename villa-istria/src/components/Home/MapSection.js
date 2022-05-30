743038d1713ecaaf608d51412a4ef30b3219142e
Aleks Markovic <aleks.markovic182@gmail.com>
2022-05-30 15:26:00 +0200

    Update map

--- villa-istria/src/components/Home/MapSection.js
+++ villa-istria/src/components/Home/MapSection.js
@@ -5,7 +5,7 @@ class Map extends Component {
     return (
       <div className="map-area">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d65148.59980886833!2d13.591983241000804!3d45.28662640249201!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1c8f9ae8b58f92f5!2zNDXCsDE3JzM2LjIiTiAxM8KwMzYnNDUuMSJF!5e1!3m2!1shr!2shr!4v1650135408710!5m2!1shr!2shr"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1119.4025610397325!2d13.611911963085207!3d45.293381990782315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477c9982c1e7761f%3A0x9f82c9aa839c03d2!2sVilla%20Istria!5e1!3m2!1shr!2shr!4v1653915252118!5m2!1shr!2shr"
           loading="lazy"
         ></iframe>
       </div>
