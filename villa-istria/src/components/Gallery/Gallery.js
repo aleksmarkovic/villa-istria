bf508739bff4c1715c651329c9063a03302a2eaf
Aleks Markovic <aleks.markovic182@gmail.com>
2022-06-16 09:57:33 +0200

    Test lazyload lib

--- villa-istria/src/components/Gallery/Gallery.js
+++ villa-istria/src/components/Gallery/Gallery.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Image from "../Common/Image";
 import LazyLoad from "react-lazyload";
+import { Table } from "react-bootstrap";
+
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const importAll = (r) => r.keys().map(r);
 
@@ -12,64 +16,72 @@ const images = importAll(
   )
 );
 
-const Spinner = () => (
-  <div className="post loading">
-    <svg
-      width="80"
-      height="80"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="xMidYMid"
-    >
-      <circle
-        cx="50"
-        cy="50"
-        fill="none"
-        stroke="#49d1e0"
-        strokeWidth="10"
-        r="35"
-        strokeDasharray="164.93361431346415 56.97787143782138"
-        transform="rotate(275.845 50 50)"
-      >
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          calcMode="linear"
-          values="0 50 50;360 50 50"
-          keyTimes="0;1"
-          dur="1s"
-          begin="0s"
-          repeatCount="indefinite"
-        />
-      </circle>
-    </svg>
-  </div>
-);
+// const Spinner = () => (
+//   <div className="post loading">
+//     <svg
+//       width="80"
+//       height="80"
+//       viewBox="0 0 100 100"
+//       preserveAspectRatio="xMidYMid"
+//     >
+//       <circle
+//         cx="50"
+//         cy="50"
+//         fill="none"
+//         stroke="#49d1e0"
+//         strokeWidth="10"
+//         r="35"
+//         strokeDasharray="164.93361431346415 56.97787143782138"
+//         transform="rotate(275.845 50 50)"
+//       >
+//         <animateTransform
+//           attributeName="transform"
+//           type="rotate"
+//           calcMode="linear"
+//           values="0 50 50;360 50 50"
+//           keyTimes="0;1"
+//           dur="1s"
+//           begin="0s"
+//           repeatCount="indefinite"
+//         />
+//       </circle>
+//     </svg>
+//   </div>
+// );
 
 const Gallery = () => {
   return (
     <main>
       <section className="blog-2 pt-120 pb-115">
         <div className="container">
+          {/* <div className="d-flex justify-content-center flex-wrap"> */}
           <div className="row">
+            {/* <Table> */}
             {images &&
               images.map((image, i) => (
                 <div
                   key={image.default.src}
-                  className="col-xxl-4 col-xl-4 col-lg-6 col-md-6 mb-30"
+                  className="col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-xs-1 mb-30"
                 >
-                  <LazyLoad
+                  <LazyLoadImage
+                    alt="image"
+                    effect="blur"
+                    src={image.default.src}
+                  />
+                  {/* <LazyLoad
                     key={i}
                     // height={10}
                     // offset={[-100, 100]}
-                    placeholder={<Spinner />}
+                    // placeholder={<Spinner />}
                     style={{ cursor: "pointer" }}
                   >
                     <Image description="image" image={image.default.src} />
-                  </LazyLoad>
+                  </LazyLoad> */}
                 </div>
               ))}
           </div>
         </div>
+        {/* </Table> */}
       </section>
     </main>
   );
