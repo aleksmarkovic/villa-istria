f431b819ad63d7a298141dc422554557c9435996
Aleks Markovic <aleks.markovic182@gmail.com>
2022-06-17 07:19:56 +0200

    Google changes

---
+++ villa-istria/src/pages/_document.js
@@ -0,0 +1,47 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+
+          {/* GTM */}
+          <Script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></Script>
+
+          <Script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WMDKJTD');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
