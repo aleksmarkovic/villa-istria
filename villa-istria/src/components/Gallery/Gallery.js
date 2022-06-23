0c367961589816fdb113b26142dd1c1e7d39f761
Aleks Markovic <aleks.markovic182@gmail.com>
2022-06-23 20:53:59 +0200

    Engage Lazyload

--- villa-istria/src/components/Gallery/Gallery.js
+++ villa-istria/src/components/Gallery/Gallery.js
@@ -16,39 +16,6 @@ const images = importAll(
   )
 );
 
-// const Spinner = () => (
-//   <div className="post loading">
-//     <svg
-//       width="80"
-//       height="80"
-//       viewBox="0 0 100 100"
-//       preserveAspectRatio="xMidYMid"
-//     >
-//       <circle
-//         cx="50"
-//         cy="50"
-//         fill="none"
-//         stroke="#49d1e0"
-//         strokeWidth="10"
-//         r="35"
-//         strokeDasharray="164.93361431346415 56.97787143782138"
-//         transform="rotate(275.845 50 50)"
-//       >
-//         <animateTransform
-//           attributeName="transform"
-//           type="rotate"
-//           calcMode="linear"
-//           values="0 50 50;360 50 50"
-//           keyTimes="0;1"
-//           dur="1s"
-//           begin="0s"
-//           repeatCount="indefinite"
-//         />
-//       </circle>
-//     </svg>
-//   </div>
-// );
-
 const Gallery = () => {
   return (
     <main>
@@ -68,17 +35,17 @@ const Gallery = () => {
                     effect="blur"
                     src={image}
                   /> */}
-                  {/* <LazyLoad
+                  <LazyLoad
                     key={i}
-                    offset={-1}
+                    offset={-100}
                     once={true}
                     // height={10}
                     // offset={[-100, 100]}
                     // placeholder={<Spinner />}
                     style={{ cursor: "pointer" }}
-                  > */}
-                  <Image description="image" image={image} />
-                  {/* </LazyLoad> */}
+                  >
+                    <Image description="image" image={image} />
+                  </LazyLoad>
                 </div>
               ))}
           </div>
