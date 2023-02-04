223dec16c39767279118a62e647c62010a544d3c
Aleks Markovic <aleks.markovic182@gmail.com>
2023-02-04 20:40:30 +0100

    Remove comments

--- villa-istria/src/pages/index.js
+++ villa-istria/src/pages/index.js
@@ -3,47 +3,7 @@ import Header from "../components/Layout/Header/Header";
 import Footer from "../components/Layout/Footer/Footer";
 import HomeMain from "../components/Home/HomeMain";
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import {
-//   getFire,
-//   collection,
-//   getDocs,
-//   query,
-// } from "firebase/fire/lite";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAWV53NbUNPUkQGo6ddg7ZQdgYqUH8eNbc",
-//   authDomain: "villa-istria.firebaseapp.com",
-//   projectId: "villa-istria",
-//   storageBucket: "villa-istria.appspot.com",
-//   messagingSenderId: "18339508284",
-//   appId: "1:18339508284:web:ec1717dc694c57e3671339",
-//   measurementId: "G-F5QZT899NR",
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFire(app);
-// const analytics = getAnalytics(app);
-
-// async function getCities() {
-//   const citiesCol = query(collection(db, "timetable"));
-//   const citySnapshot = await getDocs(citiesCol);
-//   const cityList = citySnapshot.docs.map((doc) => doc.data());
-//   console.log(cityList[0].validFrom);
-//   console.log(new Date(cityList[0].validFrom));
-
-//   return cityList;
-// }
-
 const Index = (props) => {
-  // getCities();
   return (
     <React.Fragment>
       <Header />
