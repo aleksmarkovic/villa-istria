939e33077f090947086e4b1e868e8d8662f7216c
Aleks Markovic <aleks.markovic182@gmail.com>
2023-08-10 21:01:17 +0200

    Remove unused code

--- villa-istria/src/components/Gallery/Gallery.js
+++ villa-istria/src/components/Gallery/Gallery.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Image from "../Common/Image";
 import LazyLoad from "react-lazyload";
-import { Table } from "react-bootstrap";
 
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const importAll = (r) => r.keys().map(r);
@@ -21,27 +19,17 @@ const Gallery = () => {
     <main>
       <section className="blog-2 pt-120 pb-115">
         <div className="container">
-          {/* <div className="d-flex justify-content-center flex-wrap"> */}
           <div className="row justify-content-center">
-            {/* <Table> */}
             {images &&
               images.map((image, i) => (
                 <div
                   key={image}
                   className="col-xxl-5 col-xl-5 col-lg-6 col-md-6 col-xs-1 mb-30"
                 >
-                  {/* <LazyLoadImage
-                    alt="image"
-                    effect="blur"
-                    src={image}
-                  /> */}
                   <LazyLoad
                     key={i}
                     offset={-1}
                     once={true}
-                    // height={10}
-                    // offset={[-100, 100]}
-                    // placeholder={<Spinner />}
                     style={{ cursor: "pointer" }}
                   >
                     <Image description="image" image={image} />
@@ -50,7 +38,6 @@ const Gallery = () => {
               ))}
           </div>
         </div>
-        {/* </Table> */}
       </section>
     </main>
   );
