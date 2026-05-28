ad8ebbc7f4357042f39c7c8b37c15090f20bd5ea
Aleks Markovic <aleks@histria.dev>
2026-05-28 13:37:57 +0200

    Fix map

--- src/components/Contact/ContactInfo.tsx
+++ src/components/Contact/ContactInfo.tsx
@@ -1,9 +1,6 @@
 import React, { useContext } from "react";
 import { LangContext } from "../../context/LangContext";
 
-const MAP_SRC =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1119.4025610397325!2d13.611911963085207!3d45.293381990782315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477c9982c1e7761f%3A0x9f82c9aa839c03d2!2sVilla%20Istria!5e1!3m2!1shr!2shr!4v1653915252118!5m2!1shr!2shr";
-
 const Label = ({ children }: { children: React.ReactNode }) => (
   <div
     style={{
@@ -52,21 +49,6 @@ const ContactInfo = () => {
         </div>
       </div>
 
-      <div
-        style={{
-          marginTop: "3rem",
-          width: "100%",
-          height: 260,
-          border: "1px solid var(--border)",
-          overflow: "hidden",
-        }}
-      >
-        <iframe
-          title="contact-map"
-          src={MAP_SRC}
-          style={{ width: "100%", height: "100%", border: "none" }}
-        />
-      </div>
     </div>
   );
 };
